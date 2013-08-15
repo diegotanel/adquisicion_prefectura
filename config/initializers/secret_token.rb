c0e894196a2926641e70933b14bb1bdc50331a13
Diego Tanel <diego.tanel@tecneg.com>
2013-08-15 16:07:38 -0300

    Commit inicial con la aplicación configurada

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdquisicionPrefectura::Application.config.secret_token = '6ae263c8ab8e0037d66291da3a18e8f5f3a10d1e35a7ec59cdbb33bad10458d163f4b4b1edfed3e866a2aab56abe818a514c1baedfc5a76bbd4566823124cc56'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AdquisicionPrefectura::Application.config.session_store :cookie_store, key: '_adquisicion_prefectura_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AdquisicionPrefectura::Application.config.session_store :active_record_store
